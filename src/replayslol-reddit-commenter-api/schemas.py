58a761a8e2442035ddd5d4c3dc4998cc0b5975a9
George Thomas <georgesilverbeech@gmail.com>
2023-12-20 21:49:21 +0000

    Feature - added schema for UpdateRedditComment (required for PATCH/Update ops)

--- src/replayslol-reddit-commenter-api/schemas.py
+++ src/replayslol-reddit-commenter-api/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class RedditComment(BaseModel):
@@ -18,3 +19,21 @@ class RedditComment(BaseModel):
     ready_to_be_replied_to: bool
     error: str
     media_id: str
+
+
+class UpdateRedditComment(BaseModel):
+    region: Optional[str] = None
+    summoner_name: Optional[str] = None
+    submission_id: Optional[str] = None
+    date_submitted: Optional[str] = None
+    link: Optional[str] = None
+    game_id: Optional[str] = None
+    has_been_picked_up: Optional[bool] = None
+    has_been_replied_to: Optional[bool] = None
+    patch: Optional[str] = None
+    replay_link: Optional[str] = None
+    reddit_link: Optional[str] = None
+    subreddit: Optional[str] = None
+    ready_to_be_replied_to: Optional[bool] = None
+    error: Optional[str] = None
+    media_id: Optional[str] = None
